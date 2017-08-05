c01c9000b3513ac84a8caeda8bfe59d0c6396947
Elronnd <elronnd@protonmail.ch>
2017-08-04 22:27:37 -0700

    Remove sslreconnect, and just never even try to reconnect :P

--- src/effbot.nim
+++ src/effbot.nim
@@ -6,44 +6,13 @@ const
     realname = "Goodbot 0.0.1 https://github.com/Elronnd/goodbot"
     channels = @["#esmtest"]
 
-proc sslreconnect2*(irc: AsyncIrc, timeout = 5000) {.async.} =
-    irc.sock.close()
-
-    await irc.reconnect()
-
-    let ctx = newContext(protVersion = protTLSv1, verifyMode = CVerifyNone)
-    wrapConnectedSocket(ctx, irc.sock, handshakeAsClient)
-
-proc sslreconnect*(irc: AsyncIrc, timeout = 5000) {.async.} =
-    ## Reconnects to an IRC server.
-    ##
-    ## ``Timeout`` specifies the time to wait in miliseconds between multiple
-    ## consecutive reconnections.
-    ##
-    ## This should be used when an ``EvDisconnected`` event occurs.
-
-    let secSinceReconnect = epochTime() - irc.lastReconnect
-    if secSinceReconnect < (timeout/1000):
-        await sleepAsync(timeout - int(secSinceReconnect * 1000))
-
-    irc.sock.close()
-    irc.sock = newAsyncSocket()
-
-    let ctx = newContext(protVersion = protTLSv1, verifyMode = CVerifyNone)
-#    wrapConnectedSocket(ctx, irc.sock, handshakeAsClient)
-#    wrapSocket(ctx, irc.sock)
-
-    await irc.connect()
-    wrapConnectedSocket(ctx, irc.sock, handshakeAsClient)
-    irc.lastReconnect = epochTime()
-
 
 proc onIrcEvent(client: AsyncIrc, event: IrcEvent) {.async.} =
     case event.typ
     of EvConnected:
         nil
     of EvDisconnected, EvTimeout:
-        await client.sslreconnect
+        raise newException(Exception, "Disconnected.")
     of EvMsg:
         proc reply(msg: string): Future[void] =
             client.privmsg(event.origin, msg)
@@ -63,10 +32,9 @@ proc onIrcEvent(client: AsyncIrc, event: IrcEvent) {.async.} =
             of "!quit":
                 client.close
 
-            of "!reconn":
-                await client.sslreconnect
+        echo event.raw
 
-        echo(event.raw)
+
 
 proc main() =
     stdout.write("Enter password (echoed): ")
